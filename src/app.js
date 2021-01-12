1217d5448da7eec9e68105dd635967c72d273719
m6ttw <matthewrhyswilliams@live.com>
2021-01-12 20:23:07 +0000

    exchange rates list object fetched

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ import Vue from 'vue';
 
 document.addEventListener("DOMContentLoaded", () => {
     new Vue({
-      el: "#app"
+        el: "#app",
+        data: {
+            allExchangeRates: []
+        },
+        computed: {
+
+        },
+        mounted(){
+            this.getExchangeRates()
+        },
+        methods: {
+            getExchangeRates: function(){
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(res => res.json())
+                .then(data => this.allExchangeRates = data.rates)
+            },
+        }
     })
-  })
+})
